163e95e1a4d3f18b894c3b816f40bc7590faea01
Sergey Kazakov <kazaimazai@gmail.com>
2021-05-21 18:19:50 +0300

    updated swiftui store

--- Sources/PureduxSwiftUI/EnvironmentStore.swift
+++ Sources/PureduxSwiftUI/EnvironmentStore.swift
@@ -10,26 +10,22 @@ import Combine
 import SwiftUI
 
 public class EnvironmentStore<AppState, Action>: ObservableObject {
-    private var _state: AppState
     private let store: PureduxStore.Store<AppState, Action>
     private let queue: DispatchQueue
 
     let stateSubject: PassthroughSubject<AppState, Never>
 
     var state: AppState {
-        queue.sync {
-            _state
-        }
+        store.state
     }
 
     public init(store: PureduxStore.Store<AppState, Action>,
-                queue: DispatchQueue = DispatchQueue(label: "EnvironmentStoreQueue",
-                                                     qos: .userInteractive,
-                                                     attributes: .concurrent)) {
+                label: String = "EnvironmentStoreQueue",
+                qos: DispatchQoS = .userInteractive) {
+
         self.store = store
-        self._state = store.state
         self.stateSubject = PassthroughSubject()
-        self.queue = queue
+        self.queue = DispatchQueue(label: label, qos: qos)
 
         store.subscribe(observer: asObserver)
     }
@@ -39,13 +35,15 @@ public class EnvironmentStore<AppState, Action>: ObservableObject {
     }
 
     private var asObserver: PureduxStore.Observer<AppState> {
-        PureduxStore.Observer(queue: queue) { [weak self] in
+        PureduxStore.Observer { [weak self] state in
             guard let self = self else {
                 return .dead
             }
 
-            self._state = $0
-            self.stateSubject.send($0)
+            self.queue.async {
+                self.stateSubject.send(state)
+            }
+
             return .active
         }
     }
