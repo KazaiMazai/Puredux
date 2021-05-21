2ebb8d3766be2e352dbc6a3bc52062f8f7b00c26
Sergey Kazakov <kazaimazai@gmail.com>
2021-05-21 18:03:55 +0300

    improved safety for store.state

--- Sources/PureduxStore/Store.swift
+++ Sources/PureduxStore/Store.swift
@@ -9,30 +9,36 @@ import Dispatch
 
 public class Store<State, Action> {
     public typealias Reducer = (inout State, Action) -> Void
-    public private(set) var state: State
+    private var _state: State
 
-    private let queue = DispatchQueue(label: "Store serial queue", qos: .userInitiated)
+    private let queue = DispatchQueue(label: "Store serial queue", qos: .userInitiated, attributes: .concurrent)
     private let reducer: Reducer
     private var observers: Set<Observer<State>> = []
 
     public init(initial state: State, reducer: @escaping Reducer) {
         self.reducer = reducer
-        self.state = state
+        self._state = state
+    }
+
+    public var state: State {
+        queue.sync {
+            _state
+        }
     }
 
     public func dispatch(action: Action) {
-        queue.async { [weak self] in
+        queue.async(flags: .barrier) { [weak self] in
             guard let self = self else {
                 return
             }
 
-            self.reducer(&self.state, action)
+            self.reducer(&self._state, action)
             self.observers.forEach { self.notify($0) }
         }
     }
 
     public func subscribe(observer: Observer<State>) {
-        queue.async {
+        queue.async(flags: .barrier) {
             self.observers.insert(observer)
             self.notify(observer)
         }
@@ -41,7 +47,7 @@ public class Store<State, Action> {
 
 extension Store {
     private func notify(_ observer: Observer<State>) {
-        let status = observer.observe(state)
+        let status = observer.observe(_state)
         if status == .dead {
             observers.remove(observer)
         }
