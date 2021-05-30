74a994f7375b6e0be283e7aed06baf94dc765419
Sergey Kazakov <kazaimazai@gmail.com>
2021-05-30 23:49:54 +0300

    removed state property from store, made dispatch queue serial

--- Sources/PureduxStore/Store.swift
+++ Sources/PureduxStore/Store.swift
@@ -9,36 +9,31 @@ import Dispatch
 
 public class Store<State, Action> {
     public typealias Reducer = (inout State, Action) -> Void
-    private var _state: State
 
-    private let queue = DispatchQueue(label: "Store serial queue", qos: .userInitiated, attributes: .concurrent)
+    private var state: State
+
+    private let queue = DispatchQueue(label: "StoreQueue", qos: .userInitiated)
     private let reducer: Reducer
     private var observers: Set<Observer<State>> = []
 
     public init(initial state: State, reducer: @escaping Reducer) {
         self.reducer = reducer
-        self._state = state
-    }
-
-    public var state: State {
-        queue.sync {
-            _state
-        }
+        self.state = state
     }
 
     public func dispatch(action: Action) {
-        queue.async(flags: .barrier) { [weak self] in
+        queue.async { [weak self] in
             guard let self = self else {
                 return
             }
 
-            self.reducer(&self._state, action)
+            self.reducer(&self.state, action)
             self.observers.forEach { self.notify($0) }
         }
     }
 
     public func subscribe(observer: Observer<State>) {
-        queue.async(flags: .barrier) {
+        queue.async {
             self.observers.insert(observer)
             self.notify(observer)
         }
@@ -47,7 +42,7 @@ public class Store<State, Action> {
 
 extension Store {
     private func notify(_ observer: Observer<State>) {
-        let status = observer.observe(_state)
+        let status = observer.observe(state)
         if status == .dead {
             observers.remove(observer)
         }
