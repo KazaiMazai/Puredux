1f8b7c08483e3f90f7cc957f6fbe7284250b30dd
Sergey Kazakov <kazaimazai@gmail.com>
2021-06-09 00:28:04 +0300

    updated package dependencies

--- Package.swift
+++ Package.swift
@@ -17,15 +17,15 @@ let package = Package(
     dependencies: [
         .package(name: "PureduxSideEffects",
                  url: "https://github.com/KazaiMazai/PureduxSideEffects.git",
-                 .branch("dev")),
+                 .exact("1.0.0-beta.2")),
 
         .package(name: "PureduxNetworkOperator",
                  url: "https://github.com/KazaiMazai/PureduxNetworkOperator.git",
-                 .branch("dev")),
+                 .exact("1.0.0-beta.2")),
 
         .package(name: "PureduxStore",
                  url: "https://github.com/KazaiMazai/PureduxStore.git",
-                 .branch("dev"))
+                 .exact("1.0.0-beta.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
