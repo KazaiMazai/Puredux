3ab1aea8ee0144d063e4978e29d915e2a5952f07
Sergey Kazakov <kazaimazai@gmail.com>
2021-12-06 19:22:19 +0300

    added test for test state

---
+++ Tests/PureduxStoreTests/TestStateTests.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Kazakov on 06.12.2021.
+//
+
+import XCTest
+@testable import PureduxStore
+
+final class TestStateTests: XCTestCase {
+    
+    func test_WhenStateInitWithValue_ThenIndexEquals() {
+        let initialValue = 1
+        let state = TestState(currentIndex: initialValue)
+
+        XCTAssertEqual(state.currentIndex, initialValue)
+    }
+
+    func test_WhenUpdateIndex_ThenIndexEqualsUpatedValye() {
+        let initialIndex = 1
+        let newValue = 5
+        var state = TestState(currentIndex: initialIndex)
+
+        state.reduce(action: UpdateIndex(index: newValue))
+
+        XCTAssertEqual(state.currentIndex, newValue)
+    }
+}
+
+extension TestStateTests {
+    static var allTests = [
+        ("test_WhenStateInitWithValue_ThenIndexEquals",
+         test_WhenStateInitWithValue_ThenIndexEquals),
+
+        ("test_WhenUpdateIndex_ThenIndexEqualsUpatedValye",
+         test_WhenUpdateIndex_ThenIndexEqualsUpatedValye)
+    ]
+}
