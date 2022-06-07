93e538d3ee3a5b2e412193570e775f9d178c871c
Sergey Kazakov <kazaimazai@gmail.com>
2022-06-07 14:46:07 +0100

    updated xctest manifest file

--- Tests/PureduxUIKitTests/XCTestManifests.swift
+++ Tests/PureduxUIKitTests/XCTestManifests.swift
@@ -3,7 +3,16 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(PureduxUIKitTests.allTests),
+        testCase(PresentationQueuePropsEvaluationTests.allTests),
+        testCase(PresentationQueueVCUpdateTests.allTests),
+
+        testCase(VCWithStoreWithAlwaysEqualDeduplicationPropsTests.allTests),
+        testCase(VCWithStoreWithDeduplicationPropsTests.allTests),
+        testCase(VCWithStoreWithoutDeduplicationPropsTests.allTests),
+
+        testCase(VCWithStoreWithAlwaysEqualDeduplicationVCTests),
+        testCase(VCWithStoreWithDeduplicationVCTests.allTests),
+        testCase(VCWithStoreWithoutDeduplicationVCTests.allTests)
     ]
 }
 #endif
