575778e136e42b36fdcf12441572c10b3f887f2e
Sergey Kazakov <kazaimazai@gmail.com>
2024-02-12 00:28:26 +0000

    minor fix

--- Package.swift
+++ Package.swift
@@ -15,9 +15,7 @@ let package = Package(
             name: "Puredux",
             targets: ["Puredux"]),
     ],
-    dependencies: [
-        
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
