18dffb66ee2bb06137c374647d8884ff622d942d
Sergey Kazakov <kazaimazai@gmail.com>
2024-03-28 14:06:17 +0000

    simplified Presenter

--- Sources/Puredux/UIKit/Presentable.swift
+++ Sources/Puredux/UIKit/Presentable.swift
@@ -1,12 +1,11 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Sergey Kazakov on 02.12.2020.
 //
 
-
-
+import Dispatch
 
 public protocol PresenterProtocol {
     func subscribeToStore()
@@ -14,26 +13,35 @@ public protocol PresenterProtocol {
 
 public protocol Presentable: AnyObject {
     associatedtype Props
-
+    
     var presenter: PresenterProtocol? { get set }
-
+    
     func setProps(_ props: Props)
 }
 
 public extension Presentable {
-
     func with<State, Action>(store: Store<State, Action>,
                              props: @escaping (State, Store<State, Action>) -> Self.Props,
                              presentationQueue: PresentationQueue = .sharedPresentationQueue,
                              removeStateDuplicates equating: Equating<State> = .neverEqual) {
-
-        let presenter = Presenter(
-            viewController: self,
+ 
+        let weakStore = store.store()
+        presenter = Presenter(
             store: store,
-            props: props,
-            presentationQueue: presentationQueue,
-            removeStateDuplicates: equating.predicate)
-
-        self.presenter = presenter
+            observer: Observer(
+                self,
+                removeStateDuplicates: equating, 
+                observe: { state, complete in
+                    presentationQueue.dispatchQueue.async {
+                        let props = props(state, weakStore)
+                        
+                        PresentationQueue.main.dispatchQueue.async { [weak self] in
+                            self?.setProps(props)
+                            complete(.active)
+                        }
+                    }
+                }
+            )
+        )
     }
 }
--- Sources/Puredux/UIKit/Presenter.swift
+++ Sources/Puredux/UIKit/Presenter.swift
@@ -7,87 +7,12 @@
 
 import Dispatch
 
-struct Presenter<State, Action, ViewController> where ViewController: Presentable {
-
-    private let mainQueue = DispatchQueue.main
-    private let workerQueue: DispatchQueue
-
-    private weak var viewController: ViewController?
-    private let store: Store<State, Action>
-
-    private let prevState: Ref<State?> = Ref(value: nil)
-
-    private let props: (_ state: State, _ store: Store<State, Action>) -> ViewController.Props
-    private let isEqual: (State, State) -> Bool
-
-    init(viewController: ViewController,
-         store: Store<State, Action>,
-         props: @escaping (State, Store<State, Action>) -> ViewController.Props,
-         presentationQueue: PresentationQueue,
-         removeStateDuplicates isEqual: @escaping (State, State) -> Bool) {
-
-        self.viewController = viewController
-        self.store = store
-        self.props = props
-        self.workerQueue = presentationQueue.dispatchQueue
-        self.isEqual = isEqual
-    }
-}
-
-extension Presenter: PresenterProtocol {
+struct Presenter<State, Action>: PresenterProtocol {
+    let store: Store<State, Action>
+    let observer: Observer<State>
+     
     func subscribeToStore() {
-        store.subscribe(observer: asObserver)
+        store.subscribe(observer: observer)
     }
 }
-
-private extension Presenter {
-    var asObserver: Observer<State> {
-        let store = store.store()
-        return Observer { state, complete in
-            workerQueue.async { [weak viewController] in
-                guard let viewController else {
-                    complete(.dead)
-                    return
-                }
-
-                if isPrevStateEqualTo(state) {
-                    complete(.active)
-                    return
-                }
-
-                prevState.value = state
-                let newProps = props(state, store)
-
-                mainQueue.async { [weak viewController] in
-                    guard let viewController else {
-                        complete(.dead)
-                        return
-                    }
-
-                    viewController.setProps(newProps)
-                    complete(.active)
-                }
-            }
-        }
-    }
-}
-
-private extension Presenter {
-    func isPrevStateEqualTo(_ state: State) -> Bool {
-        guard let prevState = prevState.value else {
-            return false
-        }
-
-        return isEqual(prevState, state)
-    }
-}
-
-private extension Presenter {
-    final class Ref<T> {
-        var value: T
-
-        init(value: T) {
-            self.value = value
-        }
-    }
-}
+ 
