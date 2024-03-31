f8ca8f379c1a7df80821a31cd541e37660fe325f
Sergey Kazakov <kazaimazai@gmail.com>
2024-03-31 14:25:59 +0100

    Store Observer improvements: Deduplication and Reference Type observer (#8)
    * added queue typealias
    
    * added equality check for optional values
    
    * implemented deduplicating observer
    
    * implemented deduplicating observer
    
    * added observer deduplication tests
    
    * observer optimization when no deduplication is applied
    
    * fixed deduplication
    
    * minor refactoring
    
    * minor refactoring

--- Sources/Puredux/Store/Observer.swift
+++ Sources/Puredux/Store/Observer.swift
@@ -13,37 +13,47 @@ public enum ObserverStatus {
     case dead
 }
 
-/// Observer can be subscribed to Store to handle state updates
-///
-public struct Observer<State>: Hashable {
-
+public extension Observer {
     ///     Closure that handles  Observer's status
     ///     - Parameter  status: observer status to handle
     ///
-    public typealias StatusHandler = (_ status: ObserverStatus) -> Void
-
+    typealias StatusHandler = (_ status: ObserverStatus) -> Void
+    
     ///     Observer's main closure that handle State changes and calls complete handler
-    ///        - Parameter state: new observed State
+    ///        - Parameter state: newly observed State
     ///        - Parameter complete: complete handler that Observer calls when the work is done
     ///
-    public typealias StateHandler = (_ state: State, _ complete: @escaping  StatusHandler) -> Void
+    typealias StateHandler = (_ state: State, _ complete: @escaping  StatusHandler) -> Void
 
+    ///     Observer's main closure that handle State changes and calls complete handler
+    ///        - Parameter state: newly observed State
+    ///        - Parameter prev: previous State
+    ///        - Parameter complete: complete handler that Observer calls when the work is done
+    ///
+    typealias StateObserver = (_ state: State, _ prev: State?, _ complete: @escaping  StatusHandler) -> Void
+}
+
+/// Observer can be subscribed to Store to handle state updates
+///
+public struct Observer<State>: Hashable {
     let id: UUID
-
-    private let observeClosure: StateHandler
-
-    init(id: UUID, observe: @escaping (State, @escaping StatusHandler) -> Void) {
-        self.id = id
-        self.observeClosure = observe
-    }
-
+    
+    private let stateHandler: StateObserver
+    private let removeStateDuplicates: Equating<State>?
+    private let prevState: Referenced<State?> = Referenced(value: nil)
+    
     func send(_ state: State, complete: @escaping StatusHandler) {
-        observeClosure(state, complete)
+        guard removesStateDuplicates else {
+            stateHandler(state, nil, complete)
+            return
+        }
+        let prev = prevState.value
+        prevState.value = state
+        stateHandler(state, prev, complete)
     }
 }
 
 public extension Observer {
-
     ///     Initializes a new store Observer
     ///
     ///     - Parameter observe:Is a closure that receive state and StatusHandler as parameters.
@@ -54,8 +64,19 @@ public extension Observer {
     ///     Observer handles new states, sent by the Store.
     ///
     init(observe: @escaping StateHandler) {
-        id = UUID()
-        self.observeClosure = observe
+        self.init(id: UUID(), observe: observe)
+    }
+    ///     Initializes a new store Observer
+    ///
+    ///     - Parameter observe:Is a closure that receive state and StatusHandler as parameters.
+    ///     When Observer decides to unsubscribe from store it passes .dead status parameter to status handler.
+    ///
+    ///     - Returns: Observer
+    ///
+    ///     Observer handles new states, sent by the Store.
+    ///
+    init(observe: @escaping StateObserver) {
+        self.init(id: UUID(), observe: observe)
     }
 }
 
@@ -63,8 +84,65 @@ public extension Observer {
     static func == (lhs: Observer<State>, rhs: Observer<State>) -> Bool {
         lhs.id == rhs.id
     }
-
+    
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
+    }
+}
+
+extension Observer {
+    init(id: UUID = UUID(), observe: @escaping StateHandler) {
+        self.id = id
+        self.removeStateDuplicates = nil
+        self.stateHandler = { state, _, complete in observe(state, complete) }
+    }
+    
+    init(id: UUID = UUID(), observe: @escaping StateObserver) {
+        self.id = id
+        self.removeStateDuplicates = nil
+        self.stateHandler = observe
+    }
+    
+    init(_ observer: AnyObject,
+         id: UUID = UUID(),
+         removeStateDuplicates equating: Equating<State>? = nil,
+         observe: @escaping StateHandler) {
+        
+        self.id = id
+        self.removeStateDuplicates = equating
+        self.stateHandler = { [weak observer] state, prevState, complete in
+            guard observer != nil else {
+                complete(.dead)
+                return
+            }
+            
+            guard let equating else {
+                observe(state, complete)
+                return
+            }
+            
+            guard !equating.isEqual(state, to: prevState) else {
+                complete(.active)
+                return
+            }
+            
+            observe(state, complete)
+        }
+    }
+}
+
+private extension Observer {
+    var removesStateDuplicates: Bool {
+        removeStateDuplicates != nil
+    }
+}
+
+private extension Observer {
+    final class Referenced<T> {
+        var value: T
+        
+        init(value: T) {
+            self.value = value
+        }
     }
 }
