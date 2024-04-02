e5352ac374258768b3a722a41d61285c030d6651
Sergey Kazakov <kazaimazai@gmail.com>
2024-04-02 18:16:54 +0100

    implemented store map, flatMap, compactMap

---
+++ Sources/Puredux/Store/Store+Sugar.swift
@@ -0,0 +1,62 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Kazakov on 02/04/2024.
+//
+
+import Foundation
+
+extension Store {
+    func map<T>(_ keyPath: KeyPath<State, T>) -> Store<T, Action> {
+        map { $0[keyPath: keyPath] }
+    }
+    
+    func map<T>(_ transform: @escaping (State) -> T) -> Store<T, Action> {
+        Store<T, Action>(
+            dispatch: dispatch,
+            subscribe: { localStateObserver in
+                subscribe(observer: Observer<State>(id: localStateObserver.id) { state, complete in
+                    let localState = transform(state)
+                    localStateObserver.send(localState, complete: complete)
+                })
+            }
+        )
+    }
+    
+    func compactMap<T>(_ keyPath: KeyPath<State, T?>) -> Store<T, Action> {
+        compactMap { $0[keyPath: keyPath] }
+    }
+    
+    func compactMap<T>(_ transform: @escaping (State) -> T?) -> Store<T, Action> {
+        Store<T, Action>(
+            dispatch: dispatch,
+            subscribe: { localStateObserver in
+                subscribe(observer: Observer<State>(id: localStateObserver.id) { state, complete in
+                    guard let localState = transform(state) else {
+                        complete(.active)
+                        return
+                    }
+                    
+                    localStateObserver.send(localState, complete: complete)
+                })
+            }
+        )
+    }
+    
+    func flatMap<T>(_ keyPath: KeyPath<State, T?>) -> Store<T?, Action> {
+        flatMap { $0[keyPath: keyPath] }
+    }
+    
+    func flatMap<T>(_ transform: @escaping (State) -> T?) -> Store<T?, Action> {
+        Store<T?, Action>(
+            dispatch: dispatch,
+            subscribe: { localStateObserver in
+                subscribe(observer: Observer<State>(id: localStateObserver.id) { state, complete in
+                    let localState = transform(state)
+                    localStateObserver.send(localState, complete: complete)
+                })
+            }
+        )
+    }
+}
