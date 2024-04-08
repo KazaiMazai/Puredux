1dec16b46c9af6fa32ac13e35bc9034ef8dfbfbc
Sergey Kazakov <kazaimazai@gmail.com>
2024-04-08 20:40:16 +0100

    added 4 stores test

--- Tests/PureduxTests/StoreTests/StoreNodeTests/StorePerformanceTests.swift
+++ Tests/PureduxTests/StoreTests/StoreNodeTests/StorePerformanceTests.swift
@@ -101,5 +101,48 @@ final class SingleStorePerformanceTests: XCTestCase {
             wait(for: [expectation], timeout: 30)
         }
     }
+    
+    func test_ChainOf4StoresDispatchPerfomance() {
+        measure(metrics: [XCTClockMetric(), XCTCPUMetric()]) {
+            
+            let rootStore = StateStore<Array<Int>, Int>(
+                initialState: Array(repeating: 0, count: 2000),
+                reducer: { state, action in state = state.map { $0 + action } }
+            )
+            
+            let childOne = rootStore.appending(Array(repeating: 0, count: 2000),
+                                               reducer: { state, action in state = state.map { $0 + action } }
+            )
+            
+            let childTwo = childOne.appending(Array(repeating: 0, count: 2000),
+                                              reducer: { state, action in state = state.map { $0 + action } }
+            )
+            
+            let childThree = childTwo.appending(Array(repeating: 0, count: 2000),
+                                                reducer: { state, action in state = state.map { $0 + action } }
+            )
+            
+            let expectation = expectation(description: "finished")
+            expectation.assertForOverFulfill = false
+            
+            childThree.subscribe(observer: Observer { (state, prevState, complete) in
+                guard state.0.0.0.first == 10000,
+                      state.0.0.1.first == 10000,
+                      state.0.1.first == 10000,
+                      state.1.first == 10000
+                else {
+                    return
+                }
+                
+                expectation.fulfill()
+            })
+            
+            Array(repeating: 1, count: 10000).forEach {
+                childThree.dispatch($0)
+            }
+            
+            wait(for: [expectation], timeout: 30)
+        }
+    }
 }
 
