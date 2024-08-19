fe587ca8b4c7f704fe0dc8122617a324fc3b7183
Sergey Kazakov <kazaimazai@gmail.com>
2024-08-20 00:13:47 +0100

    added store extension for testing delayed dispatch

---
+++ Tests/PureduxTests/TestUtils/StoreExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Kazakov on 19/08/2024.
+//
+
+import Foundation
+import Dispatch
+@testable import Puredux
+
+extension Store {
+    func dispatch(_ action: Action, after delay: TimeInterval) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            dispatch(action)
+        }
+    }
+}
+
+extension StateStore {
+    func dispatch(_ action: Action, after delay: TimeInterval) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            dispatch(action)
+        }
+    }
+}
