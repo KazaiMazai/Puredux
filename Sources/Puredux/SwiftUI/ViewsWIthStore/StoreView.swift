3447fde16a17a209b80e55dea4979ff9d9128114
Sergey Kazakov <kazaimazai@gmail.com>
2024-08-22 17:24:54 +0100

    store view imlementation

--- Sources/Puredux/SwiftUI/ViewsWIthStore/StoreView.swift
+++ Sources/Puredux/SwiftUI/ViewsWIthStore/StoreView.swift
@@ -15,19 +15,22 @@ public struct StoreView<ViewState, Action, Props, Content: View>: View {
     let props: (ViewState, @escaping Dispatch<Action>) -> Props
     let content: (_ props: Props) -> Content
     
-    private(set) var removeStateDuplicates: Equating<ViewState> = .neverEqual
+    private(set) var removeStateDuplicates: Equating<ViewState>?
     private(set) var presentationQueue: DispatchQueue = PresentationQueue.sharedPresentationQueue.dispatchQueue
     
     @State private var currentProps: Props?
 
     public var body: some View {
         makeContent()
-            .effect(store: store,
+            .effect(on: store,
+                    withDelay: .uiDebounce,
                     removeStateDuplicates: removeStateDuplicates,
                     on: presentationQueue) { state in
-                
-                let props = props(state, store.dispatch)
-                DispatchQueue.main.async { currentProps = props }
+               
+                Effect {
+                    let props = props(state, store.dispatch)
+                    DispatchQueue.main.async { currentProps = props }
+                }
             }
     }
 }
