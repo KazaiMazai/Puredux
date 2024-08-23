b3ec7761b4379b1b1ab239d9874c92844a0c67d3
Serge Kazakov <kazaimazai@gmail.com>
2024-08-23 16:05:49 +0100

    StoreFactory deprecations (#30)
    * renamed methods
    
    * added deprecations

--- Sources/Puredux/Store/StateStore.swift
+++ Sources/Puredux/Store/StateStore.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Sergey Kazakov on 04/04/2024.
 //
@@ -12,26 +12,26 @@ public typealias StoreObject = StateStore
 
 public struct StateStore<State, Action> {
     let storeObject: any StoreProtocol<State, Action>
-
+    
     public func store() -> Store<State, Action> {
         weakStore()
     }
-
+    
     public func dispatch(_ action: Action) {
         storeObject.dispatch(action)
     }
-
+    
     public func subscribe(observer: Observer<State>) {
         storeObject.subscribe(observer: observer)
     }
 }
 
-extension StateStore {
-    init(initialState: State,
+public extension StateStore {
+    init(_ initialState: State,
          interceptor: @escaping (Action, @escaping Dispatch<Action>) -> Void = { _, _ in },
          qos: DispatchQoS = .userInteractive,
          reducer: @escaping Reducer<State, Action>) {
-
+        
         storeObject = RootStoreNode.initRootStore(
             initialState: initialState,
             interceptor: interceptor,
@@ -47,34 +47,46 @@ extension StateStore {
             dispatcher: { [weak storeObject] in storeObject?.dispatch($0) },
             subscribe: { [weak storeObject] in storeObject?.subscribe(observer: $0) })
     }
-
+    
     func strongStore() -> Store<State, Action> {
         Store<State, Action>(
             dispatcher: dispatch,
             subscribe: subscribe
         )
     }
+}
 
-    func createChildStore<LocalState, ResultState>(initialState: LocalState,
-                                                   stateMapping: @escaping (State, LocalState) -> ResultState,
-                                                   qos: DispatchQoS,
-                                                   reducer: @escaping Reducer<LocalState, Action>) -> StateStore<ResultState, Action> {
-
-        StateStore<ResultState, Action>(
-            storeObject: storeObject.createChildStore(
-                initialState: initialState,
-                stateMapping: stateMapping,
-                qos: qos,
-                reducer: reducer
-            )
-        )
-    }
+public extension StateStore {
     
-    func createChildStore<LocalState, ResultState, LocalAction>(initialState: LocalState,
-                                                   stateMapping: @escaping (State, LocalState) -> ResultState,
-                                                   actionsMapping: ActionsMapping<Action, LocalAction>,
-                                                   reducer: @escaping Reducer<LocalState, LocalAction>) -> StateStore<ResultState, LocalAction> {
+    func stateStore<LocalState, ResultState>(
+        _ initialState: LocalState,
+        stateMapping: @escaping (State, LocalState) -> ResultState,
+        qos: DispatchQoS,
+        reducer: @escaping Reducer<LocalState, Action>
+    
+    ) -> StateStore<ResultState, Action> {
+            
+            StateStore<ResultState, Action>(
+                storeObject: storeObject.createChildStore(
+                    initialState: initialState,
+                    stateMapping: stateMapping,
+                    qos: qos,
+                    reducer: reducer
+                )
+            )
+        }
+}
 
+extension StateStore {
+    
+    func stateStore<LocalState, ResultState, LocalAction>(
+        _ initialState: LocalState,
+        stateMapping: @escaping (State, LocalState) -> ResultState,
+        actionsMapping: ActionsMapping<Action, LocalAction>,
+        reducer: @escaping Reducer<LocalState, LocalAction>
+        
+    ) -> StateStore<ResultState, LocalAction> {
+        
         StateStore<ResultState, LocalAction>(
             storeObject: storeObject.createChildStore(
                 initialState: initialState,
@@ -84,25 +96,31 @@ extension StateStore {
             )
         )
     }
-
-    func appending<T>(_ state: T,
-                      qos: DispatchQoS = .userInitiated,
-                      reducer: @escaping Reducer<T, Action>) -> StateStore<(State, T), Action> {
-
-        createChildStore(
-            initialState: state,
+    
+    public func stateStore<T>(
+        _ state: T,
+        qos: DispatchQoS = .userInitiated,
+        reducer: @escaping Reducer<T, Action>
+        
+    ) -> StateStore<(State, T), Action> {
+        
+        stateStore(
+            state,
             stateMapping: { ($0, $1) },
             qos: qos,
             reducer: reducer
         )
     }
     
-    func appending<T, A>(_ state: T,
-                         actionsMapping: ActionsMapping<Action, A>,
-                         reducer: @escaping Reducer<T, A>) -> StateStore<(State, T), A> {
-
-        createChildStore(
-            initialState: state,
+    func stateStore<T, A>(
+        _ state: T,
+        actionsMapping: ActionsMapping<Action, A>,
+        reducer: @escaping Reducer<T, A>
+        
+    ) -> StateStore<(State, T), A> {
+        
+        stateStore(
+            state,
             stateMapping: { ($0, $1) },
             actionsMapping: actionsMapping,
             reducer: reducer
