94aa03c642d1f8db50ffbb082839390e48513dff
Sergey Kazakov <kazaimazai@gmail.com>
2024-08-25 21:10:50 +0100

    fixed packages dependency

--- Package.swift
+++ Package.swift
@@ -32,6 +32,7 @@ let package = Package(
        .target(
             name: "Puredux",
             dependencies: [
+                "PureduxMacros",
                 "PureduxMacrosPlugin"
             ]),
         
