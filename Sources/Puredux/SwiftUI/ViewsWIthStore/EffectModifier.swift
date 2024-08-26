c4610733ef96def91a3eda06cadc3e9e96116be1
Sergey Kazakov <kazaimazai@gmail.com>
2024-08-27 00:59:44 +0100

    use shared UIBindings for StoreView

--- Sources/Puredux/SwiftUI/ViewsWIthStore/EffectModifier.swift
+++ Sources/Puredux/SwiftUI/ViewsWIthStore/EffectModifier.swift
@@ -10,14 +10,14 @@ import SwiftUI
 
 struct EffectModifier<ViewState, Action>: ViewModifier {
     let store: Store<ViewState, Action>
-    let createEffect: (AnyObject, Store<ViewState, Action>) -> Void
+    let createEffect: (UIStateObserver, Store<ViewState, Action>) -> Void
     
-    private class ViewIsAlive { }
+    private class StateObserver: UIStateObserver { }
     
-    @State private var viewIsAlive = ViewIsAlive()
+    @State private var stateObserver = StateObserver()
     
     func body(content: Content) -> some View {
         content
-            .onAppear { createEffect(viewIsAlive, store) }
+            .onAppear { createEffect(stateObserver, store) }
     }
 }
--- Sources/Puredux/SwiftUI/ViewsWIthStore/StoreView.swift
+++ Sources/Puredux/SwiftUI/ViewsWIthStore/StoreView.swift
@@ -9,29 +9,28 @@ import SwiftUI
 import Dispatch
 import Combine
 
-
 public struct StoreView<ViewState, Action, Props, Content: View>: View {
     let store: Store<ViewState, Action>
     let props: (ViewState, @escaping Dispatch<Action>) -> Props
     let content: (_ props: Props) -> Content
     
     private(set) var removeStateDuplicates: Equating<ViewState>?
-    private(set) var presentationQueue: DispatchQueue = PresentationQueue.sharedPresentationQueue.dispatchQueue
+    private(set) var presentationQueue: DispatchQueue = .sharedPresentationQueue
     
     @State private var currentProps: Props?
 
     public var body: some View {
         makeContent()
-            .effect(on: store,
-                    withDelay: .uiDebounce,
+            .effect(store) { uiStateObserver, store in
+            
+                uiStateObserver.subscribe(
+                    store,
+                    props: props,
+                    presentationQueue: presentationQueue,
                     removeStateDuplicates: removeStateDuplicates,
-                    on: presentationQueue) { state in
-               
-                Effect {
-                    let props = props(state, store.dispatch)
-                    DispatchQueue.main.async { currentProps = props }
-                }
-            }
+                    observe: { props in currentProps = props  }
+                )
+        }
     }
 }
 
@@ -73,7 +72,6 @@ public extension StoreView {
         self.content = content
     }
 }
-
 
 public extension StoreView where Props == (ViewState, Dispatch<Action>)  {
      
