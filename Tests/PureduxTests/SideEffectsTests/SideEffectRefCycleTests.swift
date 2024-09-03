4449c206d3fde05395532e544c752e08fb98f61d
Sergey Kazakov <kazaimazai@gmail.com>
2024-09-03 20:16:51 +0100

    updated side effects tests

--- Tests/PureduxTests/SideEffectsTests/SideEffectRefCycleTests.swift
+++ Tests/PureduxTests/SideEffectsTests/SideEffectRefCycleTests.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Sergey Kazakov on 27/08/2024.
 //
@@ -10,9 +10,9 @@ import XCTest
 @testable import Puredux
 import Puredux
 
-final class SideEffectRefCycleTests: XCTestCase {
+final class SideEffectRefCycleWithCancellableTests: XCTestCase {
     
-    func test_WhenEffectWithCancellables_ThenReferencCycleNotCreated() {
+    func test_WhenCancellableIsNotReferenced_ThenStateIsDeallocated() {
         assertDeallocated {
             var cancellable = AnyCancellableEffect()
             
@@ -20,109 +20,114 @@ final class SideEffectRefCycleTests: XCTestCase {
             let store = StateStore<ReferenceTypeState, Int>(object) {_,_ in }
                 .with(true) { _, _ in }
                 .map { (state: $0.0, boolValue: $0.1)}
-                
-        
+            
             store.effect(cancellable, toggle: \.boolValue) { state, _ in
                 
                 Effect {
                     store.dispatch(10)
                 }
             }
-           
+            
             return object as AnyObject
         }
     }
     
-    func test_WhenViewStoreIsNotReferenced_ThenReferencCycleNotCreated() {
+    func test_WhenStoreIsNotReferenced_ThenStateIsDeallocated() {
         assertDeallocated {
             let object = ReferenceTypeState()
-            
-            let viewStore = ViewStore { cancellable in
-                StateStore<ReferenceTypeState, Int>(object) {_,_ in }
-                    .with(true) { _, _ in }
-                    .map { (state: $0.0, boolValue: $0.1)}
-                    .effect(cancellable, toggle: \.boolValue) { state, dispatch in
+            let cancellable = AnyCancellableEffect()
+            let store = StateStore<ReferenceTypeState, Int>(object) {_,_ in }
+                .with(true) { _, _ in }
+                .map { (state: $0.0, boolValue: $0.1)}
+                .effect(cancellable, toggle: \.boolValue) { state, dispatch in
                     
-                        Effect {
-                            dispatch(10)
-                        }
+                    Effect {
+                        dispatch(10)
                     }
-            }
-  
+                }
+            
             return object as AnyObject
         }
     }
     
-    func test_WhenViewStoreIsReferenced_ThenReferencIsKept() {
-        var viewStore: ViewStore<Store<(state: ReferenceTypeState, boolValue: Bool), Int>>?
-       
+    func test_WhenCancellableIsReferenced_ThenReferenceIsKept() {
+        let cancellable = AnyCancellableEffect()
         assertNotDeallocated {
             let object = ReferenceTypeState()
-            
-            viewStore = ViewStore { cancellable in
-                StateStore<ReferenceTypeState, Int>(object) {_,_ in }
-                    .with(true) { _, _ in }
-                    .map { (state: $0.0, boolValue: $0.1)}
-                    .effect(cancellable, toggle: \.boolValue) { state, dispatch in
+            let store = StateStore<ReferenceTypeState, Int>(object) {_,_ in }
+                .with(true) { _, _ in }
+                .map { (state: $0.0, boolValue: $0.1)}
+                .effect(cancellable, toggle: \.boolValue) { state, dispatch in
                     
-                        Effect {
-                            dispatch(10)
-                        }
+                    Effect {
+                        dispatch(10)
                     }
-            }
-  
+                }
+            
             return object as AnyObject
         }
     }
     
-    func test_WhenCancellableIsReferenced_ThenReferencIsKept() {
-        var referenceCancellable: AnyCancellableEffect?
-        
-        assertNotDeallocated {
-            let object = ReferenceTypeState()
-            
-            let viewStore = ViewStore { cancellable in
-                referenceCancellable = cancellable
-                return StateStore<ReferenceTypeState, Int>(object) {_,_ in }
-                    .with(true) { _, _ in }
-                    .map { (state: $0.0, boolValue: $0.1)}
-                    .effect(cancellable, toggle: \.boolValue) { state, dispatch in
-                    
-                        Effect {
-                            dispatch(10)
-                        }
-                    }
-            }
-  
-            return object as AnyObject
-        }
-    }
-    
-    func test_WhenCancellableIsReferencedButCancelld_ThenReferencIsNotKept() {
-        var referencedCancellable: AnyCancellableEffect?
+    func test_WhenCancellableIsReferencedButCancelled_ThenStateIsDeallocated() {
+        let cancellable = AnyCancellableEffect()
         
         assertDeallocated {
             let object = ReferenceTypeState()
+            let store =  StateStore<ReferenceTypeState, Int>(object) {_,_ in }
+                .with(true) { _, _ in }
+                .map { (state: $0.0, boolValue: $0.1)}
+                .effect(cancellable, toggle: \.boolValue) { state, dispatch in
+                    
+                    Effect {
+                        dispatch(10)
+                    }
+                }
             
-            let viewStore = ViewStore { cancellable in
-                referencedCancellable = cancellable
+            cancellable.cancel()
+            return object as AnyObject
+        }
+    }
+}
+
+final class SideEffectRefCycleTests: XCTestCase {
+    
+    func test_WhenAccidentalRefToStoreEffect_ThenStateIsNotDeallocated() {
+        assertNotDeallocated {
+            let object = ReferenceTypeState()
+            let store = StateStore<ReferenceTypeState, Int>(object) {_,_ in }
+                .with(true) { _, _ in }
+                .map { (state: $0.0, boolValue: $0.1)}
+            
+            store.effect(toggle: \.boolValue) { state, _ in
                 
-                return StateStore<ReferenceTypeState, Int>(object) {_,_ in }
-                    .with(true) { _, _ in }
-                    .map { (state: $0.0, boolValue: $0.1)}
-                    .effect(cancellable, toggle: \.boolValue) { state, dispatch in
-                    
-                        Effect {
-                            dispatch(10)
-                        }
-                    }
+                Effect {
+                    store.dispatch(10) // reference to the store in effect
+                }
             }
             
-            referencedCancellable?.cancel()
             return object as AnyObject
         }
     }
     
+    func test_WhenStoreIsReferenced_ThenStateInNotDeallocated() {
+        var refToStore: StateStore<(state:ReferenceTypeState, boolValue: Bool), Int>?
+        assertNotDeallocated {
+            let object = ReferenceTypeState()
+            let store = StateStore<ReferenceTypeState, Int>(object) {_,_ in }
+                .with(true) { _, _ in }
+                .map { (state: $0.0, boolValue: $0.1)}
+                .effect(toggle: \.boolValue) { state, dispatch in
+                    
+                    Effect {
+                        dispatch(10)
+                    }
+                }
+            
+            refToStore = store
+            return object as AnyObject
+        }
+    }
+     
     func test_WhenStoreIsNotReferenced_TheStateIsDeallocated() {
         assertDeallocated {
             let object = ReferenceTypeState()
@@ -131,12 +136,12 @@ final class SideEffectRefCycleTests: XCTestCase {
                 .with(true) { state, _ in state.toggle() }
                 .map { (state: $0.0, boolValue: $0.1)}
                 .effect(toggle: \.boolValue) { state, dispatch in
-                
+                    
                     Effect {
                         dispatch(10)
                     }
                 }
- 
+            
             return object as AnyObject
         }
     }
