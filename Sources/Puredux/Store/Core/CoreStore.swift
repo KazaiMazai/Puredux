278d3d2c181dec88ef194004a64be36cc258904e
Sergey Kazakov <kazaimazai@gmail.com>
2024-09-09 19:25:04 +0100

    minor fix

--- Sources/Puredux/Store/Core/CoreStore.swift
+++ Sources/Puredux/Store/Core/CoreStore.swift
@@ -14,7 +14,7 @@ public typealias Interceptor<Action> = (Action, @escaping Dispatch<Action>) -> V
 typealias StoreID = UUID
 
 final class CoreStore<State, Action>: @unchecked Sendable where State: Sendable,
-                                                                          Action: Sendable {
+                                                                Action: Sendable {
     let id: StoreID = StoreID()
 
     private static var queueLabel: String { "com.puredux.store" }
