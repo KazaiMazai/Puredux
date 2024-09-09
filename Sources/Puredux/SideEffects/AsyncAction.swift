3adb092c1e654f74c95479cdff387bf3439d08d4
Sergey Kazakov <kazaimazai@gmail.com>
2024-09-09 20:17:07 +0100

    updated async action docs

--- Sources/Puredux/SideEffects/AsyncAction.swift
+++ Sources/Puredux/SideEffects/AsyncAction.swift
@@ -8,6 +8,11 @@
 import Foundation
 import Dispatch
 
+/**
+ A protocol that defines an asynchronous action that can be dispatched and executed on a specific `DispatchQueue`.
+
+ Types conforming to `AsyncAction` represent actions that perform asynchronous tasks, such as network requests or long-running computations, and return a result upon completion.
+ */
 public protocol AsyncAction: Sendable {
     associatedtype ResultAction
     var dispatchQueue: DispatchQueue { get }
