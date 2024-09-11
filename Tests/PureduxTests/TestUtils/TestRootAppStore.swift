4fce01245b38c2bf0058700b905425e9da3cfed7
Sergey Kazakov <kazaimazai@gmail.com>
2024-09-11 10:26:25 +0100

    added conditional import

--- Tests/PureduxTests/TestUtils/TestRootAppStore.swift
+++ Tests/PureduxTests/TestUtils/TestRootAppStore.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Sergey Kazakov on 01/09/2024.
 //
-
+#if canImport(PureduxMacros)
 import XCTest
 @testable import Puredux
 import SwiftUI
@@ -14,3 +14,4 @@ import PureduxMacros
 extension Injected {
     @InjectEntry var rootStore = StateStore<Int, Int>(0) { state, action in state += action }
 }
+#endif
