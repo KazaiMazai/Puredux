e4587d482475bb8fa18a815c174c655f73829812
Sergey Kazakov <kazaimazai@gmail.com>
2024-09-17 19:55:16 +0100

    updated access synchronization in Injected

--- Sources/Puredux/Store/DI.swift
+++ Sources/Puredux/Store/DI.swift
@@ -9,29 +9,29 @@ import Foundation
 import Dispatch
 
 /** Provides access to injected dependencies. */
-public struct Injected {
-     private static let queue = DispatchQueue(label: "com.puredux.injected")
+public struct Injected: Sendable {
+    private static let queue = DispatchQueue(label: "com.puredux.injected", attributes: .concurrent)
 
     /** This is only used as an accessor to the computed properties within extensions of `InjectedValues`. */
-    private static var current = Injected()
-
+ 
     /** A static subscript for updating the `currentValue` of `InjectionKey` instances. */
-    public static subscript<K>(key: K.Type) -> K.Value where K: InjectionKey {
+    public subscript<Key>(key: Key.Type) -> Key.Value where Key: InjectionKey, Key.Value: Sendable {
         get {
-            queue.sync { key.currentValue }
+            Self.queue.sync { key.currentValue }
         }
         set {
-            queue.async(flags: .barrier) { key.currentValue = newValue }
+            Self.queue.async(flags: .barrier) { key.currentValue = newValue }
         }
     }
 
     /** A static subscript accessor for updating and references dependencies directly. */
     public static subscript<T>(_ keyPath: WritableKeyPath<Injected, T>) -> T {
         get {
-            queue.sync { current[keyPath: keyPath] }
+            Injected()[keyPath: keyPath]
         }
         set {
-            queue.async(flags: .barrier) { current[keyPath: keyPath] = newValue }
+            var instance = Injected()
+            instance[keyPath: keyPath] = newValue
         }
     }
 }
